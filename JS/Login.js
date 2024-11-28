c925e7bfb3aef818f57ddd956c921b1906477847
snor <blackkyuremwhitekyurem@gmail.com>
2024-11-28 13:23:11 +0800

    add dynamic action(not successful) at login.html

---
+++ JS/Login.js
@@ -0,0 +1,20 @@
+const Account_info = document.querySelector(".Account_info");
+const Purple_button = document.querySelector(".Purple_button");
+
+function Read_content(){
+    if(Account_info.value === ""){
+        return ;
+        
+    }
+    else{
+        console.log(Account_info.value);
+
+    }
+}
+
+Account_info.addEventListener("keyup", function(e){
+    if(e.key == "Enter"){
+        Read_content();
+    }
+});
+Purple_button.addEventListener("click", Purple_button);
