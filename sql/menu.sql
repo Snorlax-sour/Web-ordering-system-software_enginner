9e14fe5ee0452ac6d0707994e7b800ecd58098f0
snor <blackkyuremwhitekyurem@gmail.com>
2025-01-04 22:51:04 +0800

    新檔案：   financial_management.sql 		新增財務管理的table 	修改：     sql/menu.sql 		將image換成url的方式、使用文字來儲存路徑 	修改：     sql/menu_item_order.sql 		刪除註解(待修改) 目前先暫時把tag (additional_request的部份)使用同一個TEXT儲存 	修改：     sql/order_db.db 		測試，確實是把datetime的型態變成text儲存，不過時間都是UTC時間，而不是GMT+8, 推薦使用程式自己處理

--- sql/menu.sql
+++ sql/menu.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Menu (
     menu_sn INTEGER PRIMARY KEY AUTOINCREMENT,
-    menu_name TEXT UNIQUE NOT NULL,
-    menu_describe_and_material TEXT,
+    menu_name nvarchar(30) UNIQUE NOT NULL,
+    menu_describe_and_material nvarchar(80),
     menu_need_ingredient INTEGER,
     menu_money INTEGER NOT NULL,
     menu_visable INTEGER DEFAULT 1 not null,  -- 1: 顯示, 0: 不顯示
-    menu_image BLOB not null,
+    menu_image TEXT not null,
     FOREIGN KEY (menu_need_ingredient) REFERENCES Menu_need_ingredient(join_sn)
 );
